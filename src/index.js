90fec25a5a13e6fe3a061c938f8476f6ac3eda05
BorbaBruno <brunodeborba@outlook.com>
2022-11-07 14:18:54 -0300

    type of params

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
+
+app.use(express.json())
 
 /* 
 
@@ -10,28 +12,40 @@ PUT - Alterar uma informação no servidor;
 PATCH - Alterar uma informação específica;
 DELETE - Deletar uma informação no servidor;
 
+Tipos de Parâmetros
+
+Route Params => Identificar um recurso, para poder editar, deletar ou buscar o mesmo.
+Query Params => Paginação / Filtro / 
+Body Params => Objetos de inserção / alteração. (JSON)
+
 */
 
-app.get("/courses", (request, response) => {
-    return response.json(['Curso 1', 'Curso 2', 'Curso 3']);
-});
-
-app.post("/courses", (request, response) => {
-    return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']);
-});
-
-app.put("/courses/:id", (request, response) => {
-    return response.json(['Curso 5', 'Curso 2', 'Curso 3', 'Curso 4']);
+app.get('/courses', (request, response) => {
+  const query = request.query
+  console.log(query)
+  return response.json(['Curso 1', 'Curso 2', 'Curso 3'])
 })
 
-app.patch("/courses/:id", (request, response) => {
-    return response.json(['Curso 5', 'Curso 7', 'Curso 3', 'Curso 4']);
+app.post('/courses', (request, response) => {
+  const body = request.body
+  console.log(body)
+  return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4'])
 })
 
-app.delete("/courses/:id", (request, response) => {
-    return response.json(['Curso 5', 'Curso 7', 'Curso 4']);
-});
+app.put('/courses/:id', (request, response) => {
+  const { id } = request.params
+  console.log(id)
+  return response.json(['Curso 5', 'Curso 2', 'Curso 3', 'Curso 4'])
+})
 
-app.listen(3333);
+app.patch('/courses/:id', (request, response) => {
+  return response.json(['Curso 5', 'Curso 7', 'Curso 3', 'Curso 4'])
+})
 
-console.log("Aplicação rodando...");
+app.delete('/courses/:id', (request, response) => {
+  return response.json(['Curso 5', 'Curso 7', 'Curso 4'])
+})
+
+app.listen(3333)
+
+console.log('Aplicação rodando...')
